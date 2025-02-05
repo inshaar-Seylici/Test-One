743a61bbfee7294e9c53a25233f79a00d7cc6e88
inshaar-Seylici <seylici157@gmail.com>
2025-02-05 10:48:59 +0300

    Waxaan kusoo daray h1

---
+++ React/src/main.jsx
@@ -0,0 +1,28 @@
+import { createRoot } from "react-dom/client";
+let root = document.querySelector('#root')
+ 
+ 
+
+createRoot(root).render( 
+<div>
+
+ 
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+<h1> hellow, Abdihakiim </h1>
+
+</div>
+)
+ 
